24d42d535beaef1c4fb7c30b4d8380a03b36f4e9
kaito <riossart@gmail.com>
2023-11-05 23:21:55 +0100

    first commit

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=False)
 
--- web/creatingroots_authentication.py
+++ web/creatingroots_authentication.py
@@ -25,3 +25,4 @@ def signup():
     """
     return "<p>Sign up</p>"
 
+
