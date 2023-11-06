d39ebdc82c35267864152c97cff46471279274ef
kaito <riossart@gmail.com>
2023-11-06 18:29:58 +0100

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+import stripe
 
 app = Flask(__name__)
 
@@ -6,12 +7,33 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+@app.route("/cancel")
+def cancel():
+    return render_template("cancel.html")
+
+
+stripe.api_key = "sk_test_51O9WGMKnUl3LeHiq6xitimUjDwOkeop6iHlJBbQCGlRaU8ZmbxFnLJW2Y6zngeNZhROfzPAqmjHbcHEa8nyVL65i00Ah7Bz5qh"
+
 @app.route("/about")
 def about():
+    try:
+        checkout_session = stripe.checkout.Session.create(line_items = [{"price": "price_1O9WTBKnUl3LeHiqcJwEsJT1", "quantity": 1}], mode = "subscription", success_url = "/success", cancel_url = "/cancel")
+
+    except Exception as e:
+        return str(e)
+    
+    redirect("/about", code=302)
+
+ 
     return render_template("about.html")
 
 if __name__ == "__main__":
     app.run()
 
     
+
     
