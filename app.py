845cb67acb6ea7db4653a5cf6c05f6ad306e9772
kaito <riossart@gmail.com>
2023-11-06 19:36:49 +0100

    first commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect
-import stripe
+#import stripe
 
 app = Flask(__name__)
 
@@ -16,20 +16,20 @@ def cancel():
     return render_template("cancel.html")
 
 
-stripe.api_key = "sk_test_51O9WGMKnUl3LeHiq6xitimUjDwOkeop6iHlJBbQCGlRaU8ZmbxFnLJW2Y6zngeNZhROfzPAqmjHbcHEa8nyVL65i00Ah7Bz5qh"
+#stripe.api_key = "sk_test_51O9WGMKnUl3LeHiq6xitimUjDwOkeop6iHlJBbQCGlRaU8ZmbxFnLJW2Y6zngeNZhROfzPAqmjHbcHEa8nyVL65i00Ah7Bz5qh"
 
 @app.route("/about")
 def about():
-    try:
-        checkout_session = stripe.checkout.Session.create(line_items = [{"price": "price_1O9WTBKnUl3LeHiqcJwEsJT1", "quantity": 1}], mode = "subscription", success_url = "/success", cancel_url = "/cancel")
+    return render_template("about.html")
+    
+    """
+        try:
+        checkout_session = stripe.checkout.Session.create(line_items = [{"price": "price_1O9WTBKnUl3LeHiqcJwEsJT1", "quantity": 1}], mode = "subscription", success_url = "https://kns-website-test357.onrender.com/success", cancel_url = "https://kns-website-test357.onrender.com/cancel")
 
     except Exception as e:
         return str(e)
     
-    redirect("/about", code=302)
-
- 
-    return render_template("about.html")
+    redirect(checkout_session.url, code=302)"""
 
 if __name__ == "__main__":
     app.run()
@@ -37,3 +37,4 @@ if __name__ == "__main__":
     
 
     
+ 
